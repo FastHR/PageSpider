9857f73b7becbbdd2f5ed91c14cc28187d15fbda
Fast Thomas <watsont1@utas.edu.au>
2023-02-18 18:43:38 +1100

    Initial commit

---
+++ Utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #todo: generate the database
+    pass
+
+def save_words_to_database(database_path: str, words_list: list):
+    #todo: to save the words to the database
+    pass
---
+++ Utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load+urls_from_file(file_path: str):
+    #todo: add the code need to read the text file with the urls in it
+    pass
+
+def load_page(url: str):
+    #todo : add the code that reads the url
+    pass
+
+def scrape_page(page_contents: str):
+    #todo : analyze the text
+    pass
